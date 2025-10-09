e54d49e8e210c7d0df7361537e96c5b9ee96f730
vicmiralo <81368132+vicmiralo@users.noreply.github.com>
2025-10-09 10:01:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ const Presentation = () => {
                 </div>
               </div>
             </div>
-            
+            <div className="space-y-3">
               <div className="bg-gradient-to-r from-pink-50 to-pink-100 p-4 rounded-lg shadow-md border-l-4 border-pink-600">
                 <div className="flex items-center space-x-3">
                   <span className="bg-pink-600 text-white rounded-full w-8 h-8 flex items-center justify-center font-bold text-sm">
@@ -136,7 +136,7 @@ const Presentation = () => {
               </div>
 
             {/* Columna derecha */}
-              <div className="space-y-3">
+              
               <div className="bg-gradient-to-r from-cyan-50 to-cyan-100 p-4 rounded-lg shadow-md border-l-4 border-cyan-600">
                 <div className="flex items-center space-x-3">
                   <span className="bg-cyan-600 text-white rounded-full w-8 h-8 flex items-center justify-center font-bold text-sm">
